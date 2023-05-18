931442e9faa3e2b574ccf66f7f8374b5bdd4ad7b
Alexander Gamonal <alexmg_750@hotmail.com>
2023-05-17 23:14:37 -0500

    intro flask

--- dia-1/1.intro-flask.py
+++ dia-1/1.intro-flask.py
@@ -1,10 +1,45 @@
-from flask import Flask;
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/productos')
+productos = [
+    {"id": 1, "nombre": "Martillo", "precio": 15.8, "disponible": True},
+    {"id": 2, "nombre": "Serrucho", "precio": 20.5, "disponible": True},
+    {"id": 3, "nombre": "Taladro", "precio": 120.0, "disponible": False},
+]
+
+
+@app.route("/productos", methods=["GET", "POST"])
 def gestionProductos():
-    return 'Yo soy la gestion de los productos'
+    print(request.method)
+    if request.method == "GET":
+        return {"message": "Los productos son", "content": productos}
+
+    elif request.method == "POST":
+        print(request.json)
+        id = len(productos) + 1
+        data = request.json
+        data["id"] = id
+        productos.append(data)
+
+        return {"message": "Producto creado exitosamente"}
+
+
+@app.route("/producto/<int:id>", methods=["GET"])
+def gestionProducto(id):
+    print(id)
+    if request.method == "GET":
+        resultado = None
+        for producto in productos:
+            if producto["id"] == id:
+                resultado = producto
+                break
+
+        if resultado == None:
+            return {"message": "No se encontro el producto a buscar"}
+        else:
+            return {"message": "El producto es", "content": resultado}
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
