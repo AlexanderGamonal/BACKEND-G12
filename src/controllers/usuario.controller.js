5552ca1ded3caa58c840e8d1a71e11d9be1feabd
Alexander Gamonal <alexmg_750@hotmail.com>
2023-07-05 23:02:39 -0500

    uso de hash de password

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -1,7 +1,36 @@
 import { conexion } from "../base_de_datos.js";
+import { registroUsuarioDto } from "../dtos/usuario.dto.js";
+import bcrypt from "bcryptjs";
 
-export const registroUsuario = (req, res) => {
-    res.status(201).json({
-        message: "Usuario creado exitosamente"
+export const registroUsuario = async (req, res) => {
+  const { body } = req;
+  const dataValidada = registroUsuarioDto.validate(body);
+
+  if (dataValidada.error) {
+    return res.status(400).json({
+      message: "Error al crear el usuario",
+      content: dataValidada.error.details[0].message,
     });
-}
+  }
+
+  // genera un texto aleatorio que luego se combinara con mi password y creara un hash del password.
+  const Salt = await bcrypt.genSalt();
+
+  // Generamos el hash del password para almacenarlo en la base de datos.
+  const password = await bcrypt.hash(dataValidada.value.password, Salt);
+
+  try {
+    const usuarioCreado = await conexion.usuario.create({
+      data: { ...dataValidada.value, password },
+    });
+
+    res.status(201).json({
+      message: "Usuario creado exitosamente",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error al crear el usuario",
+      content: "El usuario ya existe",
+    });
+  }
+};
---
+++ src/dtos/usuario.dto.js
@@ -0,0 +1,10 @@
+import Joi from "joi";
+import { TipoUsuario } from "@prisma/client";
+
+export const  registroUsuarioDto = Joi.object({
+    nombre: Joi.string().required(),
+    apellido: Joi.string(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required().regex(/^[a-zA-Z0-9!@#]{3,20}$/), //new RegExp('^[a-zA-Z0-9!@#]{3,20}$')
+    tipoUsuario: Joi.string().regex(new RegExp(`${TipoUsuario.ADMIN}|${TipoUsuario.CLIENTE}`)).required()
+});
